6f6d5dd3e3a4a12995240f7a04cf2024ac29a6e8
bhavish agarwal <bhavishagarwal@bhavishs-MacBook-Pro.local>
2012-03-23 20:48:15 +0530

    basic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chromextension::Application.config.secret_token = '476df4bd457b53c26e070c1ff3659f9a45db3982ac72b040470cab25183c0f10700b802d09e320921eea738ada22cabbf58e695090df30fb9b08778c4ef8e185'
