f06c4cc7dbd8c3130b4da47a34e6891cf8d46571
bhavish agarwal <bhavishagarwal@bhavishs-MacBook-Pro.local>
2012-04-10 22:14:10 +0530

    done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,31 @@
 class HomeController < ApplicationController
-  
+  protect_from_forgery :except => [:uploadphoto]
+  respond_to :json
   #  Index and home landing page of the user.
   def index
     
   end
   
+  def uploadphoto
+    @photo = Photo.new(params[:photo])
+    if @photo.save
+      @photo.status = "ok"
+      respond_to do |format|
+        format.json {render json: @photo}
+      end
+    else
+      @photo.status = "error"
+      respond_to do |format|
+        format.json {render json: @photo}
+      end
+    end
+  end
+  
+  def downloadphoto
+    @photo = Photo.all.descending(:created_at).first
+    
+    respond_to do |format|
+      format.json {render json: @photo}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Chromextension::Application.routes.draw do
   devise_for :users
   resources :home
   match '/dashboard' => "home#index", :as => :user_root
+  
+  match '/uploadphoto' , to: "home#uploadphoto"
+  match '/downloadphoto', to: "home#downloadphoto"
+  
   root :to => 'home#index'
   
 end
