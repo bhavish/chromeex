48cba6666d06931e86a202e01c8e91892d27f474
bhavish <bhavish.agarwal@gmail.com>
2012-09-27 22:44:19 +0530

    error fix

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -6,4 +6,5 @@ class UploadsController < ApplicationController
   def create
      @upload = Upload.new(params[:upload] || params.delete_if{ |p| !Upload.attribute_names.include?(p) })
      render nothing: true if @upload.save
+  end
 end
